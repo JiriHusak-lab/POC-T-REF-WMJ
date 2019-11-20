6bf92751d4f4fcc4508bd08f8325917a7c0a6398
JiriHusak-lab <56121194+JiriHusak-lab@users.noreply.github.com>
2019-11-20 14:47:34 +0100

    Test kafka.js dle testu JH 002

--- src/kafka.js
+++ src/kafka.js
@@ -1,40 +1,12 @@
-// Kafka configuration
-console.log("000 Kafka base setup start"); //---------------------------------
-const kafkaHost = 'apache-kafka:9092';
-//const kafkaHost = process.env.KAFKA_HOST
-//const kafkaTopic = process.env.KAFKA_TOPIC
+const kafkaHost = process.env.KAFKA_HOST
+const kafkaTopic = process.env.KAFKA_TOPIC
 const kafka = require('kafka-node')
 const JournalRec = require('./models/journalrec')
 
-
-var KeyedMessage = kafka.KeyedMessage;
-const client = new kafka.KafkaClient({kafkaHost: 'apache-kafka:9092'});
-
-console.log("200 COnsumer part");//---------------------------------		
- var Consumer = kafka.Consumer,
-     consumer = new Consumer(
-        client,
-        [
-            { topic: 'warehouse-movement', partition: 0 }
-        ],
-        {
-            autoCommit: false
-        }
-    );	
-	
-
-console.log("210 COnsumer part - nactu");//---------------------------------	
-consumer.on('message', function (message) {
-    console.log(message);
-});	
-
-
-
-/*
 try {
     console.log('Kafka Consumer is booting up ... (kafkaHost:'+kafkaHost + ' kafkaTopic:' + kafkaTopic + ')')
-
-    const client = new kafka.KafkaClient(kafkaHost)
+    //const client = new kafka.KafkaClient(kafkaHost)
+	const client = new kafka.KafkaClient({kafkaHost: 'apache-kafka:9092'});
     const topics = [
         {
             topic: kafkaTopic, 
@@ -49,22 +21,40 @@ try {
         fromOffset: false
     }
     
-    const consumer = new kafka.Consumer(client, topics, options)
+    console.log("200 Consumer1 part JH");//---------------------------------		
+	var Consumer = kafka.Consumer,
+     consumer1 = new Consumer(
+        client,
+        [
+            { topic: 'warehouse-movement', partition: 0 }
+        ],
+        {
+            autoCommit: false
+        }
+    );
+	console.log("210 Consumer1 part - nactu");//---------------------------------
+	consumer1.on('message', function (message) {
+		console.log(message);
+    });	
 
+		
+	const consumer = new kafka.Consumer(client, topics, options)
     consumer.on('message', async (message) => {    
         const journalrec = new JournalRec(JSON.parse(message.value))
         try {
             await journalrec.save()
             console.log('Journal record saved successfully')
         }catch(e) {
-            console.log(e)
+            console.log('Consumer.on e:' + e)
         }
     })
 
     consumer.on('error', (err) => {
-        console.log('error', err)
+        console.log('Consumer.on error' + err)
     })
+	
+	
+
 }catch(e) {
     console.log(e)
 }
-*/
