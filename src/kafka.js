cfa7ad6bed4a8cacc40d53eeda27aaf1887286f3
JiriHusak-lab <56121194+JiriHusak-lab@users.noreply.github.com>
2019-11-20 14:30:20 +0100

    Test kafka.js dle testu JH 001

--- src/kafka.js
+++ src/kafka.js
@@ -1,10 +1,39 @@
-const kafkaHost = process.env.KAFKA_HOST
-const kafkaTopic = process.env.KAFKA_TOPIC
+// Kafka configuration
+console.log("000 Kafka base setup start"); //---------------------------------
+const kafkaHost = 'apache-kafka:9092';
+//const kafkaHost = process.env.KAFKA_HOST
+//const kafkaTopic = process.env.KAFKA_TOPIC
 const kafka = require('kafka-node')
 const JournalRec = require('./models/journalrec')
 
+
+var KeyedMessage = kafka.KeyedMessage;
+const client = new kafka.KafkaClient({kafkaHost: 'apache-kafka:9092'});
+
+console.log("200 COnsumer part");//---------------------------------		
+ var Consumer = kafka.Consumer,
+     consumer = new Consumer(
+        client,
+        [
+            { topic: 'warehouse-movement', partition: 0 }
+        ],
+        {
+            autoCommit: false
+        }
+    );	
+	
+
+console.log("210 COnsumer part - nactu");//---------------------------------	
+consumer.on('message', function (message) {
+    console.log(message);
+});	
+
+
+
+/*
 try {
     console.log('Kafka Consumer is booting up ... (kafkaHost:'+kafkaHost + ' kafkaTopic:' + kafkaTopic + ')')
+
     const client = new kafka.KafkaClient(kafkaHost)
     const topics = [
         {
@@ -38,3 +67,4 @@ try {
 }catch(e) {
     console.log(e)
 }
+*/
