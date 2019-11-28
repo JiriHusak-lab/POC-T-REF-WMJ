c8d9ebbe0a14ce54786a33950b1f5f2e20c5cc14
JiriHusak-lab <56121194+JiriHusak-lab@users.noreply.github.com>
2019-11-28 11:58:42 +0100

    Dockerfile update for ZRUS_TEST_PRODUCER 003

--- src/kafka.js
+++ src/kafka.js
@@ -34,18 +34,21 @@ try {
 	var mDate = new Date();
 	var mDateStr = mDate.toString('dddd MMM yyyy h:mm:ss');
 	var mMatid = mDate.toString('h:mm');
+	//var jsonmsg = {  "mnozstvi": 8,  "mvm1": "wh1",  "mvm2": "wh2",  "kmat": "mat"+mMatid+"1",  "hmotnost": 12}
 	
 	var	producer = new Producer(client),
 		km = new KeyedMessage('key', 'message'),
 		payloads = [
-			{  "mnozstvi": 8,  "mvm1": "wh1",  "mvm2": "wh2",  "kmat": "mat"+mMatid+"1",  "hmotnost": 12}
+		//{ topic: 'warehouse-movement', messages: '{"mnozstvi":8,"mvm1":"wh1","mvm2":"wh2","kmat":"mat001","hmotnost":12}', partition: 0 }
+		{ topic: 'warehouse-movement', messages: 'PEPIK', partition: 0 }
 		];
+	console.log(mDateStr + ': 110 PRED Kafka PRODUCER.on')
 	producer.on('ready', function () {
 		producer.send(payloads, function (err, data) {
 			console.log(data);
 			console.log(mDateStr + ': 009 Producer.on ready');
 		});
-		console.log("010 Producer.on ready");
+		console.log("140 Producer.on ready");
 	});
 	console.log("200 Kafka PRODUCER part END");//---------------------------------
 	
