c78969e98f3ecbfac751c76d0564feeb7227067c
JiriHusak-lab <56121194+JiriHusak-lab@users.noreply.github.com>
2019-12-03 14:56:45 +0100

    ZRUS_TEST_CONSUMER - Offset 001

--- src/kafka.js
+++ src/kafka.js
@@ -1,20 +1,19 @@
+const kafka = require('kafka-node')
 const kafkaHost = process.env.KAFKA_HOST
 const kafkaPort = process.env.KAFKA_PORT
 const kafkaHostEnv = process.env.KAFKA_HOST_ENV
 const kafkaTopic = process.env.KAFKA_TOPIC
-const kafka = require('kafka-node')
+var Consumer = kafka.Consumer;
+var Offset = kafka.Offset;
+
 const JournalRec = require('./models/journalrec')
 
 let mDate = new Date();
 let mDateStr = mDate.toString('dddd MMM yyyy h:mm:ss');
 
 try {
-    console.log(mDateStr + ': Kafka Consumer is booting up ... (ENVs: kafkaHost:' + kafkaHost + '; kafkaTopic:' + kafkaTopic + '; kafkaHostEnv:' + kafkaHostEnv + '; )');
+    console.log(mDateStr + ': Kafka Consumer is booting up ... (ENVs: kafkaHost:\"' + kafkaHost + '\"; kafkaPort:' + kafkaPort + '; kafkaTopic:' + kafkaTopic + '; kafkaHostEnv:' + kafkaHostEnv + '; )');
     
-
-    //const client = new kafka.KafkaClient(kafkaHost)
-    //const client = new kafka.KafkaClient({kafkaHost: kafkaHostEnv + ':9092'});
-    //const client = new kafka.KafkaClient({kafkaHost: 'apache-kafka:9092'});
     const client = new kafka.KafkaClient({kafkaHost: kafkaHost + ':' + kafkaPort});
     
     const topics = [
@@ -24,16 +23,27 @@ try {
         }
     ]
     const options = {
-        autoCommit: true, 
+		//groupID: 'wmj-journal',         //consumer group id
+        autoCommit: false, 
+		autoCommitIntervalsMs: 5000;
         fetchMaxWaitMs: 1000, 
-        fetchMaxBytes: 1024 * 1024, 
+		fetchMinBytes: 1,
+        fetchMaxBytes: 1024 * 1024,
+		fromOffset: true,
         encoding: 'utf8', 
         fromOffset: false
     }
     
-    const consumer = new kafka.Consumer(client, topics, options)
+    const consumer = new Consumer(client, topics, options)
+	var offset = new Offset(client)
 
-    consumer.on('message', async (message) => {    
+    consumer.on('message', async (message) => {  
+		console.log(mDateStr + ': consumer.on - listening: ')
+		// a zobrazit offset
+		offset.fetch([
+			{ topic: 'warehouse-movement' }
+			], function (err, data) {
+		console.log(mDateStr + ': Offset fetch:' + data);
 
         const journalrec = new JournalRec(JSON.parse(message.value))
         try {
@@ -41,6 +51,19 @@ try {
             mDate = new Date();
             mDateStr = mDate.toString('dddd MMM yyyy h:mm:ss');
             console.log(mDateStr + ': Journal record saved successfully: ' + message.value)
+	
+			// tady by mel byt commit;
+			console.log(mDateStr + ': Bude commit:');
+			consumer.commit(function(err, data) {
+			});
+			
+			// a zobrazit offset
+			offset.fetch([
+				{ topic: 'warehouse-movement' }
+			], function (err, data) {
+			console.log(mDateStr + ': Offset fetch:' + data);
+
+});
         }catch(e) {
             mDate = new Date();
             mDateStr = mDate.toString('dddd MMM yyyy h:mm:ss');
